20a50bb81746e7d850c0f93f2a745f17d31ed8b6
Starlee Jiles <starlee.jiles@icloud.com>
2025-02-04 22:14:17 +0000

    incorporate data fetching

--- app/ui/topics/[id]/page.tsx
+++ app/ui/topics/[id]/page.tsx
@@ -1,6 +1,35 @@
-export default async function Page({ params }: { params: { id: string } }) {
-  //Simulate Page loading
-  await new Promise((r) => setTimeout(r, 3000));
+import { AskQuestion } from "@/components/AskQuestion";
+import { Question } from "@/components/Question";
+import { fetchQuestions, fetchTopic } from "@/lib/data";
+import { HashtagIcon } from "@heroicons/react/24/outline";
 
-  return <div>Topic Page: {params.id}</div>;
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const topic = await fetchTopic(id);
+  const questions = await fetchQuestions(id);
+
+  if (!topic) {
+    return <div>Topic not found</div>;
+  }
+
+  return (
+    <div>
+      <h1 className="text-3xl font-black flex items-center">
+        <HashtagIcon className="h-6 w-6 mr-2" /> {topic.title}
+      </h1>
+      <AskQuestion topic={topic.id} />
+      {questions.map((question) => (
+        <Question
+          key={question.id}
+          id={question.id}
+          text={question.title}
+          votes={question.votes}
+        />
+      ))}
+    </div>
+  );
 }
