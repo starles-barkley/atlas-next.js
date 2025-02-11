7ad188edd106b8251fdda4e19df12ff94cafe3cb
Starlee Jiles <starlee.jiles@icloud.com>
2025-02-11 20:04:19 +0000

    change bcryptjs to just bcrypt

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 import { db } from "@vercel/postgres";
 import { users, topics, questions } from "../../lib/placeholder-data";
 import { revalidatePath } from "next/cache";
--- auth.ts
+++ auth.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 import { fetchUser } from "./lib/data"
 
 
