90481b2c55af1d52ddcfb1055769ea13c0f436a8
Starlee Jiles <starlee.jiles@icloud.com>
2025-02-12 23:56:42 +0000

    oops didnt push the api auth route for github auth

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,23 @@
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+
+export const authOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
